73a9655ea30d8a3e6ef584d600d4a1edaa70e942
Nitin <nitindv260@gmail.com>
2024-04-10 01:21:51 +0530

    Refactor AddProduct component and add ListProduct component

---
+++ admin_panel/src/Components/ListProduct/ListProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './listproduct.css'
+
+const ListProduct = () => {
+  return (
+    <div className='list-product'>
+      
+    </div>
+  )
+}
+
+export default ListProduct
--- admin_panel/src/Pages/Admin/admin.jsx
+++ admin_panel/src/Pages/Admin/admin.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './admin.css';
 import Sidebar from '../../Components/Sidebar/sidebar'; 
+import {Routes,Route} from 'react-router-dom';
+import AddProduct from '../../Components/AddProduct/AddProduct';
+import ListProduct from '../../Components/ListProduct/ListProduct';
+
 
 const admin = () => {
   return (
     <div className='admin'>
       <Sidebar/>
+      <Routes>
+        <Route path='/addproduct' element={<AddProduct/>}/>
+        <Route path='/listproduct' element={<ListProduct/>}/>
+      </Routes>
     </div>
   )
 }
