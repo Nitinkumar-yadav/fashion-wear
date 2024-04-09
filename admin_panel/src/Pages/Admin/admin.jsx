ebcb0caba46e7b909fb8fdf94515c64cf1856cbe
Nitin <nitindv260@gmail.com>
2024-04-09 20:09:03 +0530

    Refactor code and add new files

--- admin_panel/src/Pages/Admin/admin.jsx
+++ admin_panel/src/Pages/Admin/admin.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './admin.css';
+import Sidebar from '../../Components/Sidebar/sidebar'; 
 
 const admin = () => {
   return (
     <div className='admin'>
-      
+      <Sidebar/>
     </div>
   )
 }
