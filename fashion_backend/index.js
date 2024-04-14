64b5a4f37e713078668eea30324d86c8ac717728
Nitin <nitindv260@gmail.com>
2024-04-14 09:55:21 +0530

    Add middleware to fetch user in index.js

--- fashion_backend/index.js
+++ fashion_backend/index.js
@@ -223,9 +223,28 @@ app.get('/popularinwomen',async(req,res)=>{
     res.send(popular_in_women);
 })
 
+// creating middleware to fetch user
+const fetchUser = async (req,res,next)=>{
+    const token = req.header('auth-token');
+    if(!token){
+        res.status(401).send({errors:"Please authenticate with vaild token"})
+    }
+    else{
+        try {
+            const data = jwt.verify(token,'secret_codes');
+            res.user =  data.user;
+            consle.log(res.user);
+            next();
+        } catch (error) {
+            res.status(401).send({errors:"Please authenticate using valid token"})
+        }
+    }
+}
+
 // Creating endpoint for adding products in cartdata
-app.post('/addtocart',async(req,res)=>{
-    log
+app.post('/addtocart',fetchUser,async(req,res)=>{
+    console.log(req.body,req.user);
+     
 })
 
 app.listen(port,(error)=>{
