67f8cdc80e17f4b8936b3da2a330b8dc3ca1fa85
Nitin <nitindv260@gmail.com>
2024-04-17 16:39:17 +0530

    Description & quantity

--- admin_panel/src/Components/AddProduct/AddProduct.jsx
+++ admin_panel/src/Components/AddProduct/AddProduct.jsx
@@ -9,6 +9,8 @@ const AddProduct = () => {
     name:"",
     image:"",
     category:"women",
+    description:"",
+    quantity: "",
     new_price:"",
     old_price:"",
   })
@@ -56,17 +58,21 @@ const AddProduct = () => {
     <div className='add-product'>
       <div className="addproduct-itemfield">
         <p>Product Title</p>
-        <input value={productDetails.name} onChange={changeHandler} type='text' name='name' placeholder='Type here'/>
+        <input value={productDetails.name} onChange={changeHandler} type='text' name='name' placeholder='Type Name Title'/>
       </div>
       <div className="addproduct-price">
         <div className="addproduct-itemfield">
           <p>Price</p>
-          <input value={productDetails.old_price} onChange={changeHandler} type='text' name='old_price' placeholder='Type here'/>
+          <input value={productDetails.old_price} onChange={changeHandler} type='text' name='old_price' placeholder='Type Price'/>
         </div>
         <div className="addproduct-itemfield">
           <p> Offer Price</p>
-          <input value={productDetails.new_price} onChange={changeHandler} type='text' name='new_price' placeholder='Type here'/>
+          <input value={productDetails.new_price} onChange={changeHandler} type='text' name='new_price' placeholder='Type Discount'/>
         </div>
+      <div className="addproduct-itemfield">
+        <p> Total Quantity</p>
+        <input value={productDetails.quantity} onChange={changeHandler} type='text' name='quantity' placeholder='Type Total Quantity'/>
+      </div>
       </div>
       <div className="addproduct-itemfield">
         <p>Product Category</p>
@@ -77,12 +83,16 @@ const AddProduct = () => {
         </select>
       </div>
       <div className="addproduct-itemfield">
+        <p> Description</p>
+        <input value={productDetails.description} onChange={changeHandler} type='textarea' name='description' placeholder='Type Product Descriptions'/>
+      </div>
+      <div className="addproduct-itemfield">
         <label htmlFor='file-input'>
           <img src={image?URL.createObjectURL(image):upload_area} className='addproduct-thumnail-img' alt="" />
         </label>
         <input onChange={imageHandler} type='file' name='image' id ='file-input' hidden/>
       </div>
-      <button onClick={()=>{Add_Product()}} className='addproduct-btn'>Add</button>
+      <button onClick={()=>{Add_Product()}} className='addproduct-btn'>Add Product</button>
     </div>
   )
 }
