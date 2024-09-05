ffad51e331b17120fd4819d81691d9495c28cf37
Nitinkumar Yadav <126796871+Nitinkumar-yadav@users.noreply.github.com>
2024-09-05 08:17:52 +0000

    completed necessary changes

--- fashion_frontend/src/Components/Navbar/Navbar.jsx
+++ fashion_frontend/src/Components/Navbar/Navbar.jsx
@@ -20,7 +20,6 @@ const Navbar = () => {
     <div className='navbar'>
        <a href='/' className="nav-logo">
         <img src={logo} alt="" />
-        {/* <p>UNIQUE</p> */}
        </a>
        <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
        <ul ref={menuRef} className="nav-menu">
@@ -31,9 +30,7 @@ const Navbar = () => {
        </ul>
        <div className="nav-login-cart">
         {localStorage.getItem('auth-token')
-        ?<button onClick={()=>{
-                localStorage.removeItem('auth-token'); 
-                window.location.replace('/')}}>
+        ?<button onClick={()=>{localStorage.removeItem('auth-token'); window.location.replace('/')}}>
           Logout</button>:
           <Link to='/login'><button>Login</button></Link>}
           <Link to='/cart'><img src={cart_icon} alt="" /></Link>
