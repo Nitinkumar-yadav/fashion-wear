908a714fe5709124361729f0dac1862731bced7b
Nitinkumar Yadav <126796871+Nitinkumar-yadav@users.noreply.github.com>
2025-01-03 04:09:44 +0000

    changes

--- fashion_frontend/src/Pages/ShopCategory.jsx
+++ fashion_frontend/src/Pages/ShopCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './CSS/ShopCategory.css'
 import {ShopContext} from '../Context/ShopContext'
-// import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 import Item from '../Components/Items/Item'
 
 const ShopCategory = (props) => {
@@ -10,12 +10,12 @@ const ShopCategory = (props) => {
     <div className='shop-category'>
         <img className='shopcategory-banner' src={props.banner} alt="" />
         <div className="shopcategory-indexSort">
-          <p>
+          {/* <p>
             <span>Showing 12 products </span>
-          </p>
-          {/* <div className="shopcategory-sort">
-            {/* Sort by <img src={dropdown_icon} alt="" /> */}
-          {/* </div> */}
+          </p> */}
+           {/* <div className="shopcategory-sort">
+           Sort by <img src={dropdown_icon} alt="" />
+          </div> */}
         </div>
         <div className="shopcategory-products">
           {all_product.map((item,i)=>{
